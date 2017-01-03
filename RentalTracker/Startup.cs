e320c69aa5a63bf27d0115d248f4900df24ce695
srmah01 <srmah01@gmail.com>
2017-01-03 13:17:12 +0000

    Add project files.

---
+++ RentalTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RentalTracker.Startup))]
+namespace RentalTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
