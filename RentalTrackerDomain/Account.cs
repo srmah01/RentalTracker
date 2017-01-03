7f6c8fc3c249c21384eaf1c50d5c2a1ab44fae58
srmah01 <srmah01@gmail.com>
2017-01-03 13:49:53 +0000

    Added initial domain model

---
+++ RentalTrackerDomain/Account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalTrackerDomain
+{
+    public class Account
+    {
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+
+        public Decimal OpeningBalance { get; set; }
+    }
+}
---
+++ RentalTrackerDomain/Payee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentalTrackerDomain
+{
+    public class Payee
+    {
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+
+        public String DefaultCategory { get; set; }
+
+        public String Memo { get; set; }
+    }
+}
