8d5a727d631e4d24f649cbaaba98fb2cb8625403
srmah01 <srmah01@gmail.com>
2017-01-11 17:06:06 +0000

    Add controllers and views for the Payee, Category and Transaction entities.

---
+++ RentalTracker/Controllers/PayeesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RentalTracker.DAL;
+using RentalTracker.Domain;
+
+namespace RentalTracker.Controllers
+{
+    public class PayeesController : Controller
+    {
+        private RentalTrackerContext db = new RentalTrackerContext();
+
+        // GET: Payees
+        public ActionResult Index()
+        {
+            return View(db.Payees.ToList());
+        }
+
+        // GET: Payees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payee payee = db.Payees.Find(id);
+            if (payee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payee);
+        }
+
+        // GET: Payees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Payees/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Memo")] Payee payee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payees.Add(payee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(payee);
+        }
+
+        // GET: Payees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payee payee = db.Payees.Find(id);
+            if (payee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payee);
+        }
+
+        // POST: Payees/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Memo")] Payee payee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(payee);
+        }
+
+        // GET: Payees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payee payee = db.Payees.Find(id);
+            if (payee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payee);
+        }
+
+        // POST: Payees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payee payee = db.Payees.Find(id);
+            db.Payees.Remove(payee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
