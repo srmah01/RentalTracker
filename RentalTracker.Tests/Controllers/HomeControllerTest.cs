817b807a5de58d6d7029df09390c854a8b49aae3
srmah01 <srmah01@gmail.com>
2017-01-19 19:54:58 +0000

    Fix failing test by creating a mock service.

--- RentalTracker.Tests/Controllers/HomeControllerTest.cs
+++ RentalTracker.Tests/Controllers/HomeControllerTest.cs
@@ -8,6 +8,7 @@ using RentalTracker;
 using RentalTracker.Controllers;
 using RentalTracker.DAL;
 using RentalTracker.Models;
+using Moq;
 
 namespace RentalTracker.Tests.Controllers
 {
@@ -18,7 +19,14 @@ namespace RentalTracker.Tests.Controllers
         public void Index()
         {
             // Arrange
-            HomeController controller = new HomeController(new RentalTrackerService());
+            var mockService = new Mock<IRentalTrackerService>();
+            mockService.Setup(s => s.GetTotalOfAccountBalances()).Returns(0m);
+            mockService.Setup(s => s.GetNumberOfAccounts()).Returns(0);
+            mockService.Setup(s => s.GetNumberOfCategories()).Returns(0);
+            mockService.Setup(s => s.GetNumberOfPayees()).Returns(0);
+            mockService.Setup(s => s.GetNumberOfTransactions()).Returns(0);
+
+            HomeController controller = new HomeController(mockService.Object);
 
             // Act
             ViewResult result = controller.Index() as ViewResult;
