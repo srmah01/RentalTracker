c7fe696b417e505fa941e2fab762ed6abec2cbfe
srmah01 <srmah01@gmail.com>
2017-02-28 17:20:32 +0000

    Add a map of PayeeId to DefaultCategoryId values to the page. Add jQuery to use the map to set the value of the Category when the value of the Payee is changed. Add Please Select item as default of Select controls and allow unobtrusive validation to show a Required error message. Fix some data in the default data where the wrong Default Categoryy was used.

--- RentalTracker/Scripts/transaction-input.js
+++ RentalTracker/Scripts/transaction-input.js
@@ -4,5 +4,14 @@
         showClose: true,
         format: 'DD/MM/YYYY'
     });
+
+    $('#PayeeId').on('change', function() {
+        var payeeValue = $(this).val();
+
+        // Use a hidden select control that maps PayeeId -> DefaultCategoryId
+        $('#PayeeCategoryMap').val(payeeValue);
+        var categoryValue = $('#PayeeCategoryMap option:selected').text();
+        $('#CategoryId').val(categoryValue);
+    });
 });
 
