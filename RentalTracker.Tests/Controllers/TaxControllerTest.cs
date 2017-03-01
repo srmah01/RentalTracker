823b027ed42d12053ff189c595aa9f167aeecc70
srmah01 <srmah01@gmail.com>
2017-03-01 11:48:21 +0000

    Add Tax Report scaffolding and route from the Dashboard.

---
+++ RentalTracker.Tests/Controllers/TaxControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+using RentalTracker.DAL;
+using RentalTracker.Controllers;
+using Moq;
+
+namespace RentalTracker.Tests.Controllers
+{
+    /// <summary>
+    /// A class representing the tests of the Tax Controller Controller.
+    /// </summary>
+    [TestClass]
+    public class TaxControllerTest
+    {
+        private MockedData mockedData;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockedData = new MockedData();
+        }
+
+        [TestMethod]
+        public void CanReturnATaxReportIndexView()
+        {
+            // Arrange
+            var mockService = new Mock<IRentalTrackerService>();
+
+            TaxReportController controller = new TaxReportController(mockService.Object);
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ RentalTracker/Controllers/TaxReportController.cs
@@ -0,0 +1,35 @@
+﻿using RentalTracker.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RentalTracker.Controllers
+{
+    public class TaxReportController : Controller
+    {
+        /// <summary>
+        /// The Rental Tracker DAL service.
+        /// </summary>
+        private IRentalTrackerService rentalTrackerService;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="rentalTrackerService">The instance of the RentalTrackerService.</param>
+        public TaxReportController(IRentalTrackerService rentalTrackerService)
+        {
+            this.rentalTrackerService = rentalTrackerService;
+        }
+
+        /// <summary>
+        /// Represents the controller for the Tax Report related pages.
+        /// </summary>
+        // GET: TaxReport
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
